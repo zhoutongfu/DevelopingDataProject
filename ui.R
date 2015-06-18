2ba856dd8db2f79536f7a52b3c63fb76308a4261
zhoutongfu <zhoutongfu@users.noreply.github.com>
2015-06-18 12:33:02 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,21 +1,26 @@
 library(shiny)
+library(png)
 source("dataFile.R")
-# Define UI for application that plots random distributions 
+
+# Define UI 
 shinyUI(fluidPage(
+
+  # set bootstrap style
   theme = "bootstrap.css",
   
   # Application title
   headerPanel("Playing with Picture Compressions"),
   
-  # Sidebar with a slider input for number of observations
+  # Sidebar with a selectInput and sliderInput
   sidebarPanel(
-    # 
+    # some illustration about SVD
     br(),
     h4("Singular Value Decomposition (SVD) For Image Compressions"),
     br(),
     p("svd makes 'lossy' compression for image and recovers the image using the components from the decomposition."),
     p("To check the quality of compression via SVD, please choose a picture and the number of components. Both the original and recovered pictures are shown on the right"),
     
+    # there are 3 images available
     selectInput("picture", "Choose a picture: ",
                 choices = c("Taylor Swift", "Bruno Mars", "Warriors")),
     
@@ -31,7 +36,7 @@ shinyUI(fluidPage(
     
   ),
   
-  # Show a plot of the generated distribution
+  # Show two images
   mainPanel(
     h4("Original Black/While Picture"),
     plotOutput("original"),
